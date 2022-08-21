9975cf722d040c141ec6ab3dadd673e0baae2d17
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2022-08-21 09:45:24 +0200

    first commit

---
+++ src/test/java/com/example/converterhttp/ConverterHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.converterhttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
